7a1a1b16e7059dc463abb4fcbbcdb71c43a9b8ca
overlookmotel <theoverlookmotel@gmail.com>
2014-11-26 18:50:30 +0000

    Add permissions in db stocking

--- defaults/models/permission.js
+++ defaults/models/permission.js
@@ -12,6 +12,11 @@ module.exports = {
 			type: Sequelize.STRING(50),
 			allowNull: false,
 			unique: true
+		},
+		type: {
+			type: Sequelize.STRING(10),
+			allowNull: true,
+			unique: true
 		}
 	}
 };
