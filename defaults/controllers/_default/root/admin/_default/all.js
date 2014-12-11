bd26caa7af75c88d2bb61687127986cf9b002db7
overlookmotel <theoverlookmotel@gmail.com>
2014-12-11 01:41:55 +0000

    Comment out admin access control

--- defaults/controllers/_default/root/admin/_default/all.js
+++ defaults/controllers/_default/root/admin/_default/all.js
@@ -6,8 +6,10 @@
 // exports
 
 exports = module.exports = {
+	/*
+	//xxx access control for admin namespace should be here, but it won't work at present
 	access: function(defaultFn) {
 		return defaultFn();
-		//xxx access control for admin namespace should be here, but it won't work at present
 	}
+	*/
 };
